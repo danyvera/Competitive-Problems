0937be8fdc4e511a2d8df980630fe3c73e123a8c
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2020-08-31 16:59:20 -0500

    Create FirstBadVersion.cpp

---
+++ Mocks/Mock3/FirstBadVersion.cpp
@@ -0,0 +1,24 @@
+//Este NO ES EL CODIGO COMPLETO
+//solamente es la funcion que LeetCode nos pide
+// The API isBadVersion a nos la dan.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long low = 1, high = n;
+        
+	        while(low < high) { 
+		        long long middle= (low + high)/2;
+		        if (isBadVersion(middle) ) { 
+			        high=middle; 
+                }	
+		        else{
+			        low=middle+1;
+		        }
+		
+	        }
+            
+	    return low;
+    }
+};
